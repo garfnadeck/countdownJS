9bff1f182adfced81e87d3b4b5913032357452f4
garfnadeck <CSOKpbkqtx36>
2021-10-20 21:53:09 -0400

    update

--- app.js
+++ app.js
@@ -26,7 +26,13 @@ const giveaway = document.querySelector('.giveaway')
 const deadline = document.querySelector('.deadline')
 const items = document.querySelectorAll('.deadline-format h4')
 
-let futureDate = new Date(2021, 11, 25, 00, 5, 0)
+let tempDate = new Date()
+let tempYear = tempDate.getFullYear()
+let tempMonth = tempDate.getMonth()
+let tempDay = tempDate.getDate()
+
+// let futureDate = new Date(2021, 11, 25, 00, 5, 0)
+let futureDate = new Date(tempYear, tempMonth, tempDay + 10, 10, 5, 0)
 
 const year = futureDate.getFullYear()
 const hours = futureDate.getHours()
